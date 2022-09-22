a98214a886538eb399779d5b54c55a0b0f36bdc3
sahyung <ende.astro@gmail.com>
2022-09-22 14:23:19 +0700

    add endpoint /skus/sort

--- routes/skus.js
+++ routes/skus.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var fs = require('fs');
+var _ = require('underscore');
+var objSKU;
+fs.readFile('./public/javascripts/sku.json', 'utf8', function (err, data) {
+    if (err) throw err;
+    objSKU = JSON.parse(data);
+});
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource sku');
+const fields = [
+    "id",
+    "SKU",
+    "item_name",
+    "product__name",
+];
+
+/* GET sku listing, sort descending by param, default product__name. */
+router.get('/sort', function (req, res, next) {
+    let orderBy = 'product__name';
+    if (req.query.orderBy && fields.includes(orderBy)) {
+        orderBy = req.query.orderBy;
+    }
+
+    let sorted = _.sortBy(objSKU, orderBy).reverse();
+    res.json(sorted);
+});
+router.get('/filter', function (req, res, next) {
+    res.send('respond with a resource sku');
 });
 
 module.exports = router;
